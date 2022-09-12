ef6b2c0a7f8d62054fbdbf9466bcba5da69ec253
kristin80 <113391662+kristin80@users.noreply.github.com>
2022-09-12 15:41:57 -0600

    Add files via upload

---
+++ reStart-python-cloud9/environment/home/ec2user/environment/Lab109.py
@@ -0,0 +1,10 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(str(myValue) + " is of the data type " + str(type(myValue)))
